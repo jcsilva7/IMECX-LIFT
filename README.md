5d9e2832d184f008d4d32c62bcc8898010bed6f0
jcsilva7 <josemiguelcsilva@gmail.com>
2025-10-21 19:25:01 +0100

    Update README.md

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 LIFT project from IMECX group
 
 Developed by University of Coimbra students, this repository contains the code of the Robotics team of the project.
+
+This repository uses the SparkFun library instead of the Adafruit "official" one because of hardware constraints and a critical need for data in time.
--- test-1/test-1.ino
+++ test-1/test-1.ino
@@ -1,21 +1,45 @@
-  #include <Servo.h>
+// The purpose of this test is to use the BNO085 module to control a servo motor based on his roll angle
+
+#include <Servo.h>
 #include <Wire.h>
 #include <SparkFun_BNO08x.h>
 
-Servo testes;
-int testes_pin = 9;
+Servo servo;
+BNO09X myIMU;
+int servo_pin = 9;
 
 void setup() {
-  testes.attach(testes_pin)
+  servo.attach(servo_pin);
+  
   Serial.begin(115200);
+
+  Wire.begin();
+
+  if (!myIMU.begin()){
+    Serial.println("BNO085 not detected. Connect or end.");
+    while (1);
+  }
+
+  // Enable output in Euler angles
+  myIMU.enableRotationVector();
+  myIMU.enableEulerAngles();
+  
+  Serial.println("BNO085 Initialized!");
 
 }
 
 void loop() {
-  for (int i = 0; i < 360; i++){
-    if(i < 180) testes.write(i);
-    else testes.write(180-i);
-    delay(50);
+  if(myIMU.dataAvailable()) {
+    float roll = myIMU.getRoll();
+
+    Serial.print("Roll: ");
+    Serial.println(roll, 2);
+
+    // Write a mapped value (and also constrained to avoid mechanical issues) into the servo
+    servo.write(constrain(map(roll, -90, 90, 0, 180), 0, 180));
   }
+
+  // Small delay to stabilize readings
+  delay(50);
 
 }
